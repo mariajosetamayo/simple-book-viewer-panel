035bdc9681533f65580e335f34c2434ea76f89e5
Maria Jose Tamayo y Ortiz <mariajose.tamayo@Marias-Mac-mini.local>
2017-01-20 19:35:28 -0700

    changed book titles

--- src/reducers/reducer-books.js
+++ src/reducers/reducer-books.js
@@ -2,10 +2,10 @@
 // in its most basic, a reducer is a function which returns the values of the state piece, in this case books. This is static state.
 export default function (){
   return [
-    {title: 'JS', pages: 101},
-    {title: 'JS2', pages: 39},
-    {title: 'JS3', pages:89},
-    {title: 'JS4', pages:1290},
-    {title: 'JS5', pages:23}
+    {title: 'The Hobbit', pages: 101},
+    {title: 'The Catcher in the Rye', pages: 39},
+    {title: 'Eloquent Javascript', pages:89},
+    {title: 'The Warriors Way', pages:1290},
+    {title: 'Jane Eyre', pages:23}
   ]
 }
